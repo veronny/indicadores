03a7fc2b5f4c00562de3fb0e2f64c80dd6841711
veronny <veronny393@hotmail.com>
2022-02-09 09:44:03 -0500

    primero infancia 02

--- resources/views/primero_infancia/index.blade.php
+++ resources/views/primero_infancia/index.blade.php
@@ -15,25 +15,27 @@
         
         <div class="col-md-4">
             <div class="card mb-4 shadow-sm text-center">
-                <img src="{{ url('img/paquete_nino.png') }}" class="card-img-top">
+                <img src="{{ url('img/paquete_gestante.png') }}" class="card-img-top">
                     <div class="card-body">
                         <h5 class="card-title" style="color:#063d9b";>PAQUETE 01</h5>
+                        <p class="card-text">Mujeres con parto intitucional, que durante su gestancion recibieron el paquete integrado de servicios</p>
+                        <a href="https://app.powerbi.com/view?r=eyJrIjoiNDM2ODYxNDMtZmRhNy00YjY2LWEyMzAtODFlOThhZTA5YWQ0IiwidCI6ImQxYzM1MDFkLWE2MWUtNGJiYi04NDdhLWQ5NDRiZmFlOGRiMCIsImMiOjR9" class="btn btn-success" target="_blank">Ver mas</a>
+                    </div>
+            </div>
+        </div>
+ 
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm text-center">
+                <img src="{{ url('img/paquete_nino.png') }}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title" style="color:#063d9b";>PAQUETE 02</h5>
                         <p class="card-text">Niños con CRED, vacunacion completa, entrega de hierro y dosaje de hemglobina y cuentan con DNI</p>
                         <a href="https://app.powerbi.com/view?r=eyJrIjoiZDVhMWJlODgtMWE5Yi00NjQwLWI3ZGItMTFhN2I0ZDhmMDE3IiwidCI6ImQxYzM1MDFkLWE2MWUtNGJiYi04NDdhLWQ5NDRiZmFlOGRiMCIsImMiOjR9" class="btn btn-success" target="_blank">Ver mas</a>
                     </div>
             </div>
         </div>
 
-        <div class="col-md-4">
-            <div class="card mb-4 shadow-sm text-center">
-                <img src="{{ url('img/paquete_gestante.png') }}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title" style="color:#063d9b";>PAQUETE 02</h5>
-                        <p class="card-text">Mujeres con parto intitucional, que durante su gestancion recibieron el paquete integrado de servicios</p>
-                        <a href="https://app.powerbi.com/view?r=eyJrIjoiNDM2ODYxNDMtZmRhNy00YjY2LWEyMzAtODFlOThhZTA5YWQ0IiwidCI6ImQxYzM1MDFkLWE2MWUtNGJiYi04NDdhLWQ5NDRiZmFlOGRiMCIsImMiOjR9" class="btn btn-success" target="_blank">Ver mas</a>
-                    </div>
-            </div>
-        </div>
+
 
 
 
